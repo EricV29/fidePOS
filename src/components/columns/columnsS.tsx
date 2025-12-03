f5acd2e0228aceb804bd7882fe26b53e0a4c0276
Eric Villeda <ericjared29@gmail.com>
2025-12-02 21:16:25 -0600

    feat(54):add full sales customers page

---
+++ src/components/columns/columnsS.tsx
@@ -0,0 +1,104 @@
+import type { ColumnDef } from "@tanstack/react-table";
+
+import { Button } from "@/components/ui/button";
+import EyeIcon from "../../assets/icons/EyeIcon";
+import DeleteIcon from "../../assets/icons/DeleteIcon";
+import EditIcon from "../../assets/icons/EditIcon";
+import { partialNumberFilter } from "@/utility/table-filter";
+import { currencyFormat } from "@/utility/currencyFormat";
+
+export type Sales = {
+  id: string;
+  created_at: string;
+  category: string;
+  ccolor: string;
+  total_amount: number;
+  actions?: {
+    view?: boolean;
+    delete?: boolean;
+    edit?: boolean;
+  };
+};
+
+// Columns Recent Sales Paid
+export const columnsS: ColumnDef<Sales>[] = [
+  {
+    id: "rowNumber",
+    header: "No",
+    meta: {
+      headerClassName: "text-center",
+    },
+    cell: ({ row }) => {
+      return <div className="text-center font-semibold">{row.index + 1}</div>;
+    },
+  },
+  {
+    accessorKey: "created_at",
+    header: "Created Date",
+    meta: {
+      headerClassName: "text-center",
+    },
+    cell: ({ row }) => {
+      return <div className="text-center">{row.getValue("created_at")}</div>;
+    },
+  },
+  {
+    accessorKey: "category",
+    header: "Category",
+    cell: ({ row }) => {
+      const category = row.getValue("category") as string;
+      const ccolor = row.original.ccolor;
+      return (
+        <div style={{ background: ccolor }} className="categoryB">
+          {category.toLocaleUpperCase()}
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "total_amount",
+    header: "Amount",
+    cell: ({ row }) => {
+      const formatted = currencyFormat(Number(row.getValue("total_amount")));
+
+      return <div className="font-semibold text-[#43A047]">{formatted}</div>;
+    },
+    filterFn: partialNumberFilter,
+  },
+  {
+    id: "actions",
+    header: "Actions",
+    meta: {
+      headerClassName: "text-center",
+    },
+    cell: ({ row, table }) => {
+      const actions = table.options.meta?.actions;
+
+      const handleDescription = () => {
+        console.log("ID:", row.original.id);
+      };
+
+      return (
+        <div className="flex justify-center items-center space-x-2">
+          {actions?.view && (
+            <Button variant="outline" size="icon" onClick={handleDescription}>
+              <EyeIcon />
+            </Button>
+          )}
+
+          {actions?.edit && (
+            <Button variant="outline" size="icon" onClick={handleDescription}>
+              <EditIcon color="#F57C00" />
+            </Button>
+          )}
+
+          {actions?.delete && (
+            <Button variant="outline" size="icon" onClick={handleDescription}>
+              <DeleteIcon color="#D32F2F" />
+            </Button>
+          )}
+        </div>
+      );
+    },
+  },
+];
--- src/pages/Reports/ReportsSales.tsx
+++ src/pages/Reports/ReportsSales.tsx
@@ -1,11 +1,149 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import CardInfoNumber from "@/components/CardInfoNumber";
+import BoxIcon from "@/assets/icons/BoxIcon";
+import PayStrkeIcon from "@/assets/icons/PayStrkeIcon";
+import ChartPieDonutText from "@/components/pie-chart";
+import { addRandomFill } from "../../utility/AddFill";
+import BarChartEx from "@/components/bar-chart";
+import { DataTable } from "@/components/data-table";
+import { columnsS } from "@/components/columns/columnsS";
+
+interface PieChartItem {
+  fill: string;
+  [key: string]: string | number;
+}
+interface BarChartItem {
+  [key: string]: string | number;
+}
+
+export type Sales = {
+  id: string;
+  created_at: string;
+  category: string;
+  ccolor: string;
+  total_amount: number;
+  actions?: {
+    view?: boolean;
+    delete?: boolean;
+    edit?: boolean;
+  };
+};
+
+//* Example data pie chart
+const chartDataCSDB = [
+  { category: "Maquillaje", sales: 10 },
+  { category: "Dulces", sales: 20 },
+  { category: "Edredones", sales: 87 },
+  { category: "Zapatos", sales: 73 },
+];
+
+const chartConfigCS = {
+  sales: {
+    label: "Sales",
+  },
+};
+
+//* Example data bar chart
+const chartDataTCSDB = [
+  { category: "Maquillaje", sales: 186 },
+  { category: "Regalos", sales: 305 },
+  { category: "Edredones", sales: 237 },
+  { category: "Dulces", sales: 73 },
+  { category: "Zapatos", sales: 209 },
+];
+
+const chartConfigTCS = {
+  sales: {
+    label: "Sales",
+    color: "#1976D2",
+  },
+};
+
+//* Example data table
+const dataSBD = [
+  {
+    id: "728ed51f",
+    created_at: "16/11/2025",
+    category: "Maquillaje",
+    ccolor: "#5b49ff",
+    total_amount: 100,
+  },
+  {
+    id: "728ed52f",
+    created_at: "16/11/2025",
+    category: "toys",
+    ccolor: "#ff49ff",
+    total_amount: 50,
+  },
+];
 
 interface ReportsSalesProps {}
 
 const ReportsSales: React.FC<ReportsSalesProps> = ({}) => {
+  const [chartDataCSF, setChartDataCSF] = useState<PieChartItem[]>([]);
+  const [chartDataTCS, setChartDataTSC] = useState<BarChartItem[]>([]);
+  const [dataTableS, setDataTableS] = useState<Sales[]>([]);
+
+  useEffect(() => {
+    setChartDataCSF(addRandomFill(chartDataCSDB));
+    setChartDataTSC(chartDataTCSDB);
+    setDataTableS(dataSBD);
+  }, []);
+
   return (
     <>
-      <div className="">Reports Sales</div>
+      <div className="w-full h-full flex flex-col gap-2">
+        <div className="flex gap-2 h-[110px] overflow-x-auto overflow-y-hidden">
+          <CardInfoNumber
+            icon={BoxIcon}
+            title="Inventory value"
+            icond={null}
+            number={100000}
+            format={true}
+            color="#FFC107"
+          />
+          <CardInfoNumber
+            icon={PayStrkeIcon}
+            title="Sales: 10"
+            icond={null}
+            number={1500}
+            format={true}
+            color="#1976D2"
+          />
+        </div>
+        <div className="w-full h-auto flex-1 flex flex-col overflow-y-auto gap-2">
+          <div className="w-full flex gap-2 h-[280px]">
+            <div className="max-w-[300px] min-w-0 w-full h-full flex flex-col justify-center items-center p-5 gap-5 border-2 border-[#b3b3b3] rounded-[10px] bg-white">
+              <p className="font-semibold">Categories Sales</p>
+              <ChartPieDonutText
+                chartData={chartDataCSF}
+                chartConfig={chartConfigCS}
+              />
+            </div>
+            <div className="max-w-[600px] min-w-[400px] w-[600px] h-full flex flex-col justify-center items-start p-5 gap-5 border-2 border-[#b3b3b3] rounded-[10px] bg-white">
+              <p className="font-semibold mb-2">Sales by Category</p>
+              <BarChartEx
+                chartData={chartDataTCS}
+                chartConfig={chartConfigTCS}
+                xAxis="category"
+                yAxis="sales"
+              />
+            </div>
+          </div>
+          <div className="w-full h-[500px] p-4 gap-1 border-2 border-[#b3b3b3] rounded-[10px] bg-white flex flex-col">
+            <p className="font-semibold mb-2">Sales</p>
+            <DataTable
+              columns={columnsS}
+              data={dataTableS}
+              actions={{
+                view: true,
+                edit: false,
+                delete: false,
+              }}
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
