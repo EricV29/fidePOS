160c5eee2ce346d6424e4a33b7b636d2d11c4f6d
Eric Villeda <ericjared29@gmail.com>
2025-12-05 21:27:46 -0600

    feat(47):add signup form

--- src/pages/signup/Signup.tsx
+++ src/pages/signup/Signup.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import ProfileForm from "@/components/example-form-signup";
+import fidelogoc from "@img/fidelogoc.png";
 
 const Signup: React.FC = () => {
   const handleSignup = () => {
@@ -7,9 +9,17 @@ const Signup: React.FC = () => {
 
   return (
     <>
-      <div className="w-screen h-screen flex flex-col justify-center items-center">
-        <h1>Signup</h1>
-        <button onClick={handleSignup}>Entrar</button>
+      <div className="w-screen h-screen flex flex-col justify-center items-center overflow-hidden gap-5 absolute inset-0 z-10 bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#F57C00_100%)]">
+        <div className="w-full flex flex-col justify-center items-center">
+          <img src={fidelogoc} alt="" className="w-20" />
+          <h1>Welcome</h1>
+          <p className="font-extralight">
+            Welcome to FidePOS, your ideal point of view for you bussiness.
+          </p>
+        </div>
+        <div className="w-[450px]">
+          <ProfileForm onSuccess={handleSignup} />
+        </div>
       </div>
     </>
   );
