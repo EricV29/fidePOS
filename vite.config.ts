eb61cf266ff93698bb31613e1ca4a5a2e78fc7c0
Eric Villeda <ericjared29@gmail.com>
2026-03-17 13:49:18 -0600

    add treemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      open: true,
+      filename: "bundle-stats.html",
+    }),
+  ],
   base: "./",
   server: {
     port: 6969,
